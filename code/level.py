122325b6cc7f126bc7d4a4dbb1da003238b57ab0
Yasaman <yasamanarya80@yahoo.com>
2023-06-11 18:57:54 -0400

    simple moving camera

--- code/level.py
+++ code/level.py
@@ -25,7 +25,7 @@ class Level:
     def run(self, dt):
         self.display_surface.fill('black')
         # self.all_sprites.draw(self.display_surface)
-        self.all_sprites.custom_draw()
+        self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
         self.overlay.display()
 
@@ -34,10 +34,17 @@ class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
 
-    def custom_draw(self):
+    def custom_draw(self, player):
+        # finding the vector from center to players current position
+        # this vector is used (in reverse direction (-)) to draw everything else in the map
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
         # use the layer to draw sprites in the correct depth order
         for layer in LAYERS.values():
             for sprite in self.sprites():
                 if sprite.z == layer:
-                    self.display_surface.blit(sprite.image, sprite.rect)
+                    offset_rect = sprite.rect.copy()
+                    offset_rect.center -= self.offset
+                    self.display_surface.blit(sprite.image, offset_rect)
